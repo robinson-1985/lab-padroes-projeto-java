67d2137e117eae6414b2e4f9d0fc73f8f09f0943
robinson <robinpaulista85@gmail.com>
2022-03-07 13:45:05 -0300

    Facade

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.facade.Facade;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
@@ -40,6 +41,11 @@ public class Test {
         robo.mover();
         robo.mover();
         robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Robinson","14800409");
 
     }
 
