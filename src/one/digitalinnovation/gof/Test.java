0100a3686e7d0fd4d577c40be2b8346e4dabee78
robinson <robinpaulista85@gmail.com>
2022-03-07 11:50:52 -0300

    Strategy

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,9 +1,14 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
 public class Test {
 
     public static void main(String[] args) {
-        // Testes relacionados ao Design Pattern Singleton:
+        // Singleton:
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstancia();
@@ -18,6 +23,24 @@ public class Test {
         System.out.println(lazyHolder);
         lazy = SingletonLazy.getInstancia();
         System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
     }
 
 
